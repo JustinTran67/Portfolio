65ca12abb0e259dbf35307cadd1bc263f62e8f01
Justin Tran <justintran@Justins-MacBook-Pro.local>
2026-01-01 15:31:25 -0800

    resume bug fix

--- frontend/src/components/Resume.jsx
+++ frontend/src/components/Resume.jsx
@@ -1,11 +1,9 @@
-//Assets
-import resume from "../assets/justin_tran_resume.pdf";
 
 export default function Resume() {
 
     return(
         <div className="">
-            <iframe className="w-full h-screen" src={resume} title="Resume"></iframe>
+            <iframe className="w-full h-screen" src="/justin_tran_resume.pdf" title="Resume"></iframe>
         </div>
     )
 }
